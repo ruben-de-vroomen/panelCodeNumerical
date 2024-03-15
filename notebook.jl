1009d0318e730c90e46e6c7455fe6af261575dbb
rubendev <rubvro.45@gmail.com>
2024-03-15 11:02:18 +0100

    created the notebook

---
+++ notebook.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.40
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ eb38f380-e2b2-11ee-373f-c9d7d40ee588
+
+
+# ╔═╡ Cell order:
+# ╠═eb38f380-e2b2-11ee-373f-c9d7d40ee588
