8f674295b27859694a3a5dc7681f218da08a8dee
rubendev <rubvro.45@gmail.com>
2024-03-15 11:07:34 +0100

    started on the notebook, made a bib file for references

--- notebook.jl
+++ notebook.jl
@@ -5,7 +5,25 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ eb38f380-e2b2-11ee-373f-c9d7d40ee588
+md"""
+# Panel Method Improvements
+- Anna de Bever - 
+- Ruben de Vroomen - 5140617
+"""
 
+# ╔═╡ a72de2fb-b2d9-45a2-ae06-d373b1d6126e
+md"""
+## Introduction
+
+This project 
+"""
+
+# ╔═╡ bfcd1719-8d8b-4035-8a81-ca9db14b19d2
+md"""
+## Literature Study
+"""
 
 # ╔═╡ Cell order:
-# ╠═eb38f380-e2b2-11ee-373f-c9d7d40ee588
+# ╟─eb38f380-e2b2-11ee-373f-c9d7d40ee588
+# ╠═a72de2fb-b2d9-45a2-ae06-d373b1d6126e
+# ╠═bfcd1719-8d8b-4035-8a81-ca9db14b19d2
